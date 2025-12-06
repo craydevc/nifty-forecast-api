da9aeb93da426eb285faa53ca9d92075ac253d12
craydevc <32226827+craydevc@users.noreply.github.com>
2025-12-06 20:36:32 +0530

    Update forecast.js

--- api/forecast.js
+++ api/forecast.js
@@ -1,13 +1,3 @@
-// api/forecast.js
-export default async function handler(req, res) {
-  const key = req.query.key;
-  if(key !== "1") {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-  const imageUrl = "https://drive.google.com/uc?export=download&id=1SyH2OFBc-Lb2A-NMU01MIRU5YHo-kL5e";
-  const response = await fetch(imageUrl);
-  const buffer = await response.arrayBuffer();
-  res.setHeader("Content-Type", "image/png");
-  res.send(Buffer.from(buffer));
+export default function handler(req, res) {
+  res.status(200).send("Your Nifty Forecast API is working!");
 }
